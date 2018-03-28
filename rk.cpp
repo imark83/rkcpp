bf0fbd1defc16d142c5ca96c7b21806662e5b1ce
Marcos Rodríguez <marcos@unizar.es>
2018-03-28 13:15:16 +0200

    Veremos dijo el ciego

--- rk.cpp
+++ rk.cpp
@@ -9,9 +9,9 @@
 extern int nsteps;
 extern int nrejected;
 
-Buffer retard0(1000000, 0.0);
-Buffer retard1(1000000, 0.0);
-Buffer retard2(1000000, 0.0);
+Buffer retard0(10000, 0.0);
+Buffer retard1(10000, 0.0);
+Buffer retard2(10000, 0.0);
 
 void computeStages (int nvar, 		// number of variables
 			double rkStage[],					// RK stages
